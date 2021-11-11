bf4970b6a5d052bac715c114ea001ecfc62b8d8d
Lei Xu <leixu@leansoftx.com>
2021-11-11 19:35:19 +0800

    smoke test

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ server.on('error', onError);
 server.on('listening', onListening);
 
 console.log("Server running on port " + port);
-console.log("Sprint 6 - 941 +1");
+console.log("Smoke test");
 
 /**
  * Normalize a port into a number, string, or false.
