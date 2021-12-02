f178ade206ef2865543694f6a932ce35fdc02848
Lei Xu <leixu@leixu-macpro16>
2021-12-02 17:00:35 +0800

    smoke test

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ server.on('error', onError);
 server.on('listening', onListening);
 
 console.log("Server running on port " + port);
+console.log("Edit in smartide.");
 
 /**
  * Normalize a port into a number, string, or false.
