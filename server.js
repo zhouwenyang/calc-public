4ed2cb77ae3f89950665aa4540f38c62a1ef709c
Lei Xu <leixu@leixu-macpro16>
2021-12-28 15:10:31 +0000

    vol fix

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ server.on('error', onError);
 server.on('listening', onListening);
 
 console.log("Server running on port " + port);
-console.log("smoke test build 1689 mac-local");
+console.log("smoke test build 1689 mac-local vol-fix");
 /**
  * Normalize a port into a number, string, or false.
  */
