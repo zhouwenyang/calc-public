1798580e09a217ec78335deb1954991b393081bf
Lei Xu <leixu@leixu-macpro16>
2021-12-29 14:14:48 +0000

    smoke 1703

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ server.on('error', onError);
 server.on('listening', onListening);
 
 console.log("Server running on port " + port);
-console.log("smoke test build 1689 mac-local vol-fix");
+console.log("smoke test build 1703 mac-local");
 /**
  * Normalize a port into a number, string, or false.
  */
