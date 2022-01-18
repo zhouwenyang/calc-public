373718eb89d55c523ddfa44e2738af4aee67b50b
Lei Xu <leixu@leansoftx.com>
2022-01-18 14:42:51 +0000

    smoke 1827 windows remote

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ server.on('error', onError);
 server.on('listening', onListening);
 
 console.log("Server running on port " + port);
-console.log("smoke test build 1827 windows-local");
+console.log("smoke test build 1827 windows-local-remote");
 /**
  * Normalize a port into a number, string, or false.
  */
