1ee5aa1529c17761f82908d1bb5050a7d8546cef
Lei Xu <leixu@leansoftx.com>
2022-01-28 14:37:23 +0000

    smoke test 1970 in JetBrain Gateways

--- server.js
+++ server.js
@@ -33,8 +33,7 @@ server.on('error', onError);
 server.on('listening', onListening);
 
 console.log("Server running on port " + port);
-console.log("smoke test build 1970 win-local");
-console.log("smartide demo");
+console.log("smoke test for verison 1970");
 /**
  * Normalize a port into a number, string, or false.
  */
