354b016c3ea94ad7bc899d66268393623898bb71
Lei Xu <leixu@leixu-macpro16>
2022-02-07 08:40:04 +0000

    smoke 2020 mac local

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ server.on('error', onError);
 server.on('listening', onListening);
 
 console.log("Server running on port " + port);
-console.log("smoke test for verison 1970");
+console.log("smoke mac local 2020");
 /**
  * Normalize a port into a number, string, or false.
  */
